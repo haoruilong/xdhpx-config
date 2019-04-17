d02bf97bc536e6a117ef4f3f9b99616a61e2302c
hrl <20108515@qq.com>
2019-04-17 16:26:25 +0800

    新增作者自定义

--- xdhpx-config-web/src/main/java/com/xdhpx/config/web/SwaggerConfig.java
+++ xdhpx-config-web/src/main/java/com/xdhpx/config/web/SwaggerConfig.java
@@ -3,13 +3,14 @@ package com.xdhpx.config.web;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import io.swagger.annotations.ApiOperation;
-import springfox.documentation.service.Contact;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,12 +22,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 */
 @Configuration
 @EnableSwagger2
+@Profile({"local","dev","test"})/**在本地环境、开发环境、测试环境开启**/
 public class SwaggerConfig {
  
 	@Value("${swagger.title:docTitle}")
     private String swaggerTitle;
 	@Value("${swagger.description:docInfo}")  
     private String swaggerDescription;
+	@Value("${swagger.author:HRL}")  
+    private String swaggerAuthor;
 	
 	@Bean
     public Docket createRestApi() {
@@ -44,7 +48,7 @@ public class SwaggerConfig {
         return new ApiInfoBuilder()
                 .title(swaggerTitle)      /**标题**/
                 .description(swaggerDescription)	/**描述**/
-                .contact(new Contact("郝瑞龙", null, null))/**联系：作者，网址，邮箱**/
+                .contact(new Contact(swaggerAuthor, null, null))/**联系：作者，网址，邮箱**/
                 .termsOfServiceUrl("http://localhost:8080/swagger-ui.html")//
                 .version("1.0")/**描述**/
                 .build();
